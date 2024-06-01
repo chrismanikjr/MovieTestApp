d4fa8568ebc570ad1247fa82252df2fe7fb0dcb6
Chrismanto Admo <chrismanto.natanail@admo.astra.co.id>
2024-06-01 21:43:30 +0700

    Initial Commit

---
+++ MovieAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppTest
+//
+//  Created by Chrismanto Natanail Manik on 01/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
